c2c5f73ea04a98b503a78a1464937682d565f630
matheusburey <matheushburei.99@gmail.com>
2025-04-17 21:57:25 -0300

    feat: config expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import "../assets/global.css";
+
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "transparent" },
+          animation: "fade",
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import "./assets/global.css";
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
